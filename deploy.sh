6e757768e9bb9813f21187d0d171834d9110fcb4
502-10\Administrator <reyne7055@gmail.com>
2025-11-12 10:45:01 +0900

    22

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
+# deploy.sh
+read -p "ECR 이미지 URI 입력 (예: ACCOUNT_ID.dkr.ecr.ap-northeast-2.amazonaws.com/petclinic:latest): " IMAGE_URI
 
-
-
-
-echo -e "\n${YELLOW}[Step 5] Deployment YAML 업데이트${NC}"
+echo -e "\n${YELLOW} Deployment YAML 업데이트${NC}"
 sed -i "s|<AWS_ACCOUNT_ID>\.dkr\.ecr\..*\.amazonaws\.com/petclinic:latest|$IMAGE_URI|g" petclinic-deployment.yaml
 echo -e "${GREEN}YAML 파일 업데이트 완료${NC}"
 
 
-echo -e "\n${YELLOW}[Step 6] Kubernetes Secret 생성${NC}"
+echo -e "\n${YELLOW}Kubernetes Secret 생성${NC}"
 read -sp "RDS MySQL 비밀번호 입력: " DB_PASSWORD
 echo ""
 
@@ -21,7 +20,7 @@ else
     echo -e "${YELLOW}Secret 생성 실패${NC}"
 fi
 
-echo -e "\n${YELLOW}[Step 7] EKS 클러스터에 배포${NC}"
+echo -e "\n${YELLOW} EKS 클러스터에 배포${NC}"
 kubectl apply -f petclinic-deployment.yaml
 
 if [ $? -eq 0 ]; then
@@ -31,7 +30,7 @@ else
     exit 1
 fi
 
-echo -e "\n${YELLOW}[Step 8] 배포 상태 확인${NC}"
+echo -e "\n${YELLOW} 배포 상태 확인${NC}"
 echo "deployment 상태:"
 kubectl get deployment -n petclinic
 
